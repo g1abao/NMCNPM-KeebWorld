3953f792d0e5dd61db4acbce26d0483c78e8f7d0
Gia Bảo <dngbao.229@gmail.com>
2023-01-08 20:34:43 +0700

    database

---
+++ src/database/database.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2/promise');
+
+const db = {connection: null}
+
+(async () => {
+    // create the connection to database
+    db.connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      database: 'doan',
+      password: 'giabao229'
+  
+    });
+    console.log('Database connected!');
+  })();
+
+module.exports = db;
