72b6b9cec5b5dfeda0fcb9a6150eaab28a71e4a8
Quang030602 <minhquang030602@gmail.com>
2023-01-10 23:04:50 +0700

    fix merge conflict in productdetail

--- src/database/database.js
+++ src/database/database.js
@@ -8,7 +8,7 @@ const db = {connection: null};
       host: 'localhost',
       user: 'root',
       database: 'cnpm_keebworld',
-      password: 'giabao229'
+      password: 'Quang030602'
   
     });
     console.log('Database connected!');
