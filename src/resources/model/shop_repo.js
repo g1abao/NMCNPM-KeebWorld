0d022bab7ecc5e2da302e7f5dd68dca585356a89
Gia Bảo <dngbao.229@gmail.com>
2023-01-10 22:42:31 +0700

    routing product-detail

--- src/resources/model/shop_repo.js
+++ src/resources/model/shop_repo.js
@@ -1,7 +1,7 @@
 const db = require("../../database/database");
 
-exports.getAllProduct = async () => {
-  const result = await db.connection.execute('select * from product');
+exports.getAllProducts = async () => {
+  const result = await db.connection.execute("select * from product");
   return result[0];
 };
 
@@ -11,4 +11,12 @@ exports.filter_kit = async (category) => {
     [category]
   );
   return result[0];
+};
+
+exports.getProduct = async (id_product) => {
+  const result = await db.connection.execute(
+    "select * from product where id_product = ?",
+    [id_product]
+  );
+  return result[0][0];
 };
